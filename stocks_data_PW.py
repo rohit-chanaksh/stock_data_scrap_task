dc03665008b4b0c983735f274c88ec657a2a77d2
rohitborkar1 <167988667+rohitborkar1@users.noreply.github.com>
2024-08-12 01:38:46 +0530

    updated PW

--- stocks_data_PW.py
+++ stocks_data_PW.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup , NavigableString
-import pandas as pd
+# import pandas as pd
 
 url = 'https://in.tradingview.com/markets/stocks-india/sectorandindustry-industry/information-technology-services/'
 
@@ -13,8 +13,60 @@ def getLinks(stockPage):
     return link_extensions
 
 def stock_scrap(new_page):
-    Stock_Name = new_page.find('h1', {'class': 'apply-overflow-tooltip title-HFnhSVZy'}).getText()
-    return {'Stock_Name': Stock_Name}
+    new_pageData = new_page.inner_html('body')
+    # print(new_pageData)  # checking
+    bSoup2 = BeautifulSoup(new_pageData , 'html.parser')
+    # print(bSoup2.prettify())  # checking
+
+
+    Stock_Name = bSoup2.find('h1', {'class': 'apply-overflow-tooltip title-HFnhSVZy'}).getText()
+    Stock_Price = bSoup2.find('span', {'class': 'last-JWoJqCpY js-symbol-last'}).find('span').getText()
+
+    Market_Capitalization = bSoup2.find('div', {'data-container-name': 'key-stats-id'}).find('div', {'class': 'content-JhZ1X2FK'}).find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    key_stat_list = bSoup2.find('div', {'data-container-name': 'key-stats-id'}).find_all('div' , {'class': 'block-GgmpMpKr container-lQwbiR8R'} )
+    # print(len(key_stat_list))
+    Dividend_yield_indicated =  key_stat_list[1].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    Price_to_earnings_Ratio_TTM =  key_stat_list[2].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    Basic_EPS_TTM =  key_stat_list[3].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    Net_income_FY =  key_stat_list[4].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    Revenue_FY =  key_stat_list[5].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    Shares_float=  key_stat_list[6].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+    Beta_1Y =  key_stat_list[7].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText()
+
+
+
+    # print('Stock Name:-' ,bSoup2.find('h1', {'class': 'apply-overflow-tooltip title-HFnhSVZy'}).getText())
+    # print('Stock Price:-' ,bSoup2.find('span', {'class': 'last-JWoJqCpY js-symbol-last'}).find('span').getText())
+    # print('Market Capitalization :- ', bSoup2.find('div', {'data-container-name': 'key-stats-id'}).find('div', {'class': 'content-JhZ1X2FK'}).find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # key_stat_list = bSoup2.find('div', {'data-container-name': 'key-stats-id'}).find_all('div' , {'class': 'block-GgmpMpKr container-lQwbiR8R'} )
+    # # print(len(key_stat_list))
+    # print('Dividend yield (indicated) :-' , key_stat_list[1].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # print('Price to earnings Ratio (TTM):-' , key_stat_list[2].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # print('Basic EPS (TTM):-' , key_stat_list[3].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # print('Net income (FY):-' , key_stat_list[4].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # print('Revenue (FY):-' , key_stat_list[5].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # print('Shares float:-' , key_stat_list[6].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+    # print('Beta (1Y):-' , key_stat_list[7].find('div', {'class': 'apply-overflow-tooltip value-GgmpMpKr'}).getText())
+
+    # print('/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////')
+    
+    # print(Stock_Name)
+    # print(Stock_Price)
+
+    return {
+        'Stock_Name': Stock_Name ,
+        'Stock_Price' : Stock_Price,
+        'Market_Capitalization': Market_Capitalization ,
+        'Dividend_yield_indicated' : Dividend_yield_indicated,
+        'Price_to_earnings_Ratio_TTM': Price_to_earnings_Ratio_TTM,
+        'Basic_EPS_TTM' : Basic_EPS_TTM ,
+        'Net_income_FY' : Net_income_FY ,
+        'Revenue_FY' : Revenue_FY ,
+        'Shares_float' : Shares_float ,
+        'Beta_1Y' : Beta_1Y 
+        }
+
+
 
 if __name__ == '__main__':
     with sync_playwright() as p:
@@ -28,10 +80,15 @@ if __name__ == '__main__':
 
         pageData = page.inner_html('body')
         bSoup = BeautifulSoup(pageData, 'html.parser')
+        # print(bSoup)
 
         link_list = getLinks(bSoup)
+
         parent_URL = 'https://in.tradingview.com'
+
         stock_links = [parent_URL + extension for extension in link_list]
+
+        # print(stock_links)
 
         stock_details = []
 
@@ -42,10 +99,11 @@ if __name__ == '__main__':
             
             stock_data = stock_scrap(new_page)
             stock_details.append(stock_data)
-
-            print(stock_details)
             
             new_page.close()
+
+        print(stock_details)
+
 
         
 
